fde949d16a8dab64be7b21a09d04accdb3948b5e
Filatov <o.filatov@rendez-vous.ru>
2023-09-04 14:07:15 +0300

    Start project

---
+++ HW_1_OTUS_2023/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HW_1_OTUS_2023
+//
+//  Created by Филатов Олег Олегович on 04.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW_1_OTUS_2023/HW_1_OTUS_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_1_OTUS_2023App.swift
+//  HW_1_OTUS_2023
+//
+//  Created by Филатов Олег Олегович on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW_1_OTUS_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
